d2f117740ffaf03272e91f61144022773a48dde8
ShivPratapRaj <shivprja22@gmail.com>
2021-07-13 00:56:12 +0530

    Added runtime.txt file

--- RCOLLECTION/settings.py
+++ RCOLLECTION/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xq-z2c#=(+v68^r-01--uh$o5*==dvid&*jeia!7fvzdwu_q7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = "*"
 
 
 # Application definition
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

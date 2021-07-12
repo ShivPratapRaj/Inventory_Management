5de7211af4f756032907a17b1680b1d441c95570
ShivPratapRaj <shivprja22@gmail.com>
2021-07-13 01:50:06 +0530

    fixed static path

--- RCOLLECTION/settings.py
+++ RCOLLECTION/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-xq-z2c#=(+v68^r-01--uh$o5*==dvid&*jeia!7fvzdwu_q7$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
